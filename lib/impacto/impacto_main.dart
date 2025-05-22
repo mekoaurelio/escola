baa89e77c5dabe9d17d83478449eb001a08832c3
meko <mekoaurelio@gmail.com>
2025-05-22 08:39:52 -0300

    atualizando a pasta impacto

--- lib/impacto/impacto_main.dart
+++ lib/impacto/impacto_main.dart
@@ -21,6 +21,7 @@ class _ImpactoMainState extends State<ImpactoMain> with TickerProviderStateMixin
     setState(() => isLoading = false);
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
